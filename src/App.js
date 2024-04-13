17067305e27203f030b8d9b54edbe6b8395a3476
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-04-13 15:25:19 +0300

    files updated

--- src/App.js
+++ src/App.js
@@ -10,15 +10,7 @@ import initialTodos from './todos.json';
 import Form from 'components/Form';
 import TodoEditor from 'components/TodoEditor/TodoEditor';
 import Filter from 'components/Filter';
-
-const colorPickerOptions = [
-  { label: 'red', color: '#F44336' },
-  { label: 'green', color: '#4CAF50' },
-  { label: 'blue', color: '#2196F3' },
-  { label: 'grey', color: '#607D8B' },
-  { label: 'pink', color: '#E91E63' },
-  { label: 'indigo', color: '#3F51B5' },
-];
+import colorPickerOptions from './colorPickerOptions.json';
 
 class App extends Component {
   state = {
